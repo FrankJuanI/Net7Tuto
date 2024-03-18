bf5328b53b96bd71a532a448780d85d88c0d02af
Juan Frank <juan.frank@gm2dev.com>
2024-03-18 09:54:40 -0300

    first commit

---
+++ Net7Tuto/Program.cs
@@ -0,0 +1,33 @@
+﻿using Net7Tuto.Data;
+using Net7Tuto.Services.SuperHeroService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+builder.Services.AddDbContext<DataContext>();
+builder.Services.Configure<DataContext>(
+                 builder.Configuration.GetSection(nameof(DataContext)));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
---
+++ Net7Tuto/Services/SuperHeroService/ISuperHeroService.cs
@@ -0,0 +1,16 @@
+﻿namespace Net7Tuto.Services.SuperHeroService
+{
+	public interface ISuperHeroService
+	{
+        Task<List<SuperHero>> GetAllHeroes();
+
+        Task<SuperHero?> GetSingleHero(int id);
+
+        Task<List<SuperHero>> AddHero(SuperHero hero);
+
+        Task<List<SuperHero>?> UpdateHero(int id, SuperHero request);
+
+        Task<List<SuperHero>?> DeleteHero(int id);
+    }
+}
+
