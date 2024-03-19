2a8527a1585b8fa6922db00f2f8d694104ebd2eb
Juan Frank <juan.frank@gm2dev.com>
2024-03-19 12:10:15 -0300

    feat(Employee.cs): create the emplyee model

---
+++ Net7Tuto/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿namespace Net7Tuto.Models
+{
+	public class Employee
+	{
+		public Guid Id { get; set; }
+
+		public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Place { get; set; } = string.Empty;
+
+		public Guid AdressId { set; get; } 
+
+		public string Seniority { get; set; } = string.Empty;
+
+		public int Salary { get; set; }
+
+		public string CurrentProyect { get; set; } = string.Empty;
+    }
+
+}
+
+
+//public class EmployeesAdresses
+//{
+//	public int Id { get; set; }
+
+//	public string Street { get; set; } = string.Empty;
+
+//	public string Number { get; set; } = string.Empty;
+
+//	public string PostalCode { get; set; } = string.Empty;
+
+//	public string Province { get; set; } = string.Empty;
+
+//}
+
+//public class Proyects
+//{
+//	public int Id { get; set; }
+
+//	public string ProyectName { get; set; } = string.Empty;
+
+//	public int EmployeeID { get; set; }
+//}
