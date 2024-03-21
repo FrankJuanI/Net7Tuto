502b3a982f1866cf1232379d1e5b36bb68a678fa
Juan Frank <juan.frank@gm2dev.com>
2024-03-21 14:39:18 -0300

    feat: Department model

---
+++ Net7Tuto/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Net7Tuto.Models
+{
+    public class Department
+    {
+        [Key]
+        public Guid DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; } = string.Empty;
+
+        public List<Employee>? Employees { get; set; }
+
+        //public Employee? Employee { get; set; }
+
+    }
+}
