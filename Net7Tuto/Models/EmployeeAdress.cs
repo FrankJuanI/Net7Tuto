9741e3bc7193c898ce016a6f7729dcba1aac1bff
Juan Frank <juan.frank@gm2dev.com>
2024-03-21 14:39:38 -0300

    feat:EmployeesAddress model

---
+++ Net7Tuto/Models/EmployeeAdress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Net7Tuto.Models
+{
+    public class EmployeeAdress
+    {
+        [Key]
+        public Guid AdressId { get; set; }
+
+        public string Street { get; set; } = string.Empty;
+
+        public string Number { get; set; } = string.Empty;
+
+        public string PostalCode { get; set; } = string.Empty;
+
+        public string Province { get; set; } = string.Empty;
+
+    }
+}
+
