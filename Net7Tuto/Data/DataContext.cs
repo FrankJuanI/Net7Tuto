c7684ced2b7202e598aa20944a6f99a936507bf4
Juan Frank <juan.frank@gm2dev.com>
2024-03-21 14:40:50 -0300

    feat: integrate new models to context

--- Net7Tuto/Data/DataContext.cs
+++ Net7Tuto/Data/DataContext.cs
@@ -13,9 +13,35 @@ namespace Net7Tuto.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=localhost;Database=Employees;User=sa;Password=J72016HGp?;Trusted_Connection=False;Encrypt=false");
+            optionsBuilder.UseSqlServer("Server=localhost;Database=gm2;User=sa;Password=J72016HGp?;Trusted_Connection=False;Encrypt=false");
         }
 
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeAdress> EmployeeAdresses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        //public DbSet<Client> Clients { get; set; }
+        //public DbSet<Project> Proyects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Employee>()
+            //    .HasOne(a => a.EmployeeAdress)
+            //    .WithOne(e => e.Employee)
+            //    .HasForeignKey<Employee>(a => a.EmployeeAdress);
+
+            //modelBuilder.Entity<Department>()
+            //    .HasMany(a => a.Employees)
+            //    .WithOne(e => e.Department)
+            //    .HasForeignKey(a => a.DepartmentId);
+
+            //modelBuilder.Entity<Client>()
+            //    .HasMany(a => a.Projects)
+            //    .WithOne(e => e.Client)
+            //    .HasForeignKey(a => a.ClientId);
+
+            //modelBuilder.Entity<Project>()
+            //    .HasMany(e => e.Employees)
+            //    .WithMany(a => a.Projects);
+        }
     }
 }
