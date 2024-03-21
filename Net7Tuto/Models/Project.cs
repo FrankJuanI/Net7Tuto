2fc202d3fbe915abca01c8d1d478b1babcf149d5
Juan Frank <juan.frank@gm2dev.com>
2024-03-21 14:39:55 -0300

    feat: projects model

---
+++ Net7Tuto/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Net7Tuto.Models
+{
+    public class Project
+    {
+        [Key]
+        public Guid ProjectId { get; set; }
+
+        public string ProjectName { get; set; } = string.Empty;
+
+        public Guid ClientId { get; set; } 
+
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
+    }
+}
+
