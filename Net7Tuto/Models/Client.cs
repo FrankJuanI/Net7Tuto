dda1a39daa1569bf1eea2be39d86d3f0289ffa1b
Juan Frank <juan.frank@gm2dev.com>
2024-03-21 14:38:57 -0300

    feat: Client model

---
+++ Net7Tuto/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Net7Tuto.Models
+{
+    public class Client
+    {
+        [Key]
+        public Guid ClientId { get; set; }
+
+        public string ClientName { get; set; } = string.Empty;
+
+        public ICollection<Project> Projects { get; set; } = new List<Project>();
+
+    }
+}
+
