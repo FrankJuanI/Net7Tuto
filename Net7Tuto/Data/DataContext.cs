7aef6cdb7c6e8446207957eec28971ce70c29f8a
JuanI02 <juanifrankk01@gmail.com>
2024-03-22 14:17:43 -0300

    feat: add Departments dbset & delete comments

--- Net7Tuto/Data/DataContext.cs
+++ Net7Tuto/Data/DataContext.cs
@@ -13,35 +13,18 @@ namespace Net7Tuto.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=localhost;Database=gm2;User=sa;Password=J72016HGp?;Trusted_Connection=False;Encrypt=false");
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=gm4;Trusted_Connection=True;TrustServerCertificate=true");
         }
 
         public DbSet<Employee> Employees { get; set; }
         public DbSet<EmployeeAdress> EmployeeAdresses { get; set; }
         public DbSet<Department> Departments { get; set; }
+
         //public DbSet<Client> Clients { get; set; }
         //public DbSet<Project> Proyects { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<Employee>()
-            //    .HasOne(a => a.EmployeeAdress)
-            //    .WithOne(e => e.Employee)
-            //    .HasForeignKey<Employee>(a => a.EmployeeAdress);
-
-            //modelBuilder.Entity<Department>()
-            //    .HasMany(a => a.Employees)
-            //    .WithOne(e => e.Department)
-            //    .HasForeignKey(a => a.DepartmentId);
-
-            //modelBuilder.Entity<Client>()
-            //    .HasMany(a => a.Projects)
-            //    .WithOne(e => e.Client)
-            //    .HasForeignKey(a => a.ClientId);
-
-            //modelBuilder.Entity<Project>()
-            //    .HasMany(e => e.Employees)
-            //    .WithMany(a => a.Projects);
         }
     }
 }
